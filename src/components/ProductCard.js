394185799eb4fa1cac39e196deb3d5dee95aaa7d
Puja Anand <puu5598@gmail.com>
2024-10-12 00:46:50 +0530

    Remove folder

---
+++ src/components/ProductCard.js
@@ -0,0 +1,28 @@
+import styles from "./Product.module.css";
+import Image from 'next/image';
+import { useState } from "react";
+const ProductCard = ({ product }) => {
+    const [isClick, setisClick] = useState(false)
+    if (!product) {
+        return null;
+    }
+    return (
+
+        <div className={styles.productCard}>
+            <img src={product.image} alt={product.title} className={styles.productImage} />
+            <h2 className={styles.productTitle}>{product.title}</h2>
+            <p className={styles.product}><a href="/sign" className={styles.signIn}>Sign in</a>  or create an account to see pricing</p>
+            <span onClick={() => setisClick(!isClick)}>
+                {!isClick ? (
+                    <Image src="/heart.png" alt="heart" width={24} height={24} />
+                ) : (
+                    <Image src="/heart.svg" alt="Favorite" width={24} height={24} />
+                )}
+            </span>
+        </div>
+
+    )
+
+
+}
+export default ProductCard;
